78f4d826f78b81cd7ff34d79db8daf8cf4245e15
Bishobos <mps.kajkage@gmail.com>
2025-09-17 11:35:38 +0200

    Done

--- src/library/LoanItemsTest.java
+++ src/library/LoanItemsTest.java
@@ -1,12 +1,24 @@
 package library;
 
-
+/**
+ * LoanItemTest contains our main method and hence runs the program.
+ * <strong>Has Methods</strong><br>
+ * {@link #main(String[])}<br>
+ * {@link #mainWrapper()}
+ */
 
 public class LoanItemsTest {
+    /**
+     * main is the main string and runs the program.
+     * @param args
+     */
     public static void main(String[] args){
         mainWrapper();
     }
 
+    /**
+     * mainWrapper contains all the valid code and exists to keep {@link #main(String[])} as empty as possible.
+     */
     public static void mainWrapper(){
         InventoryHelper helper = new InventoryHelper();
         LoanItems[] items = helper.makeInventory();
