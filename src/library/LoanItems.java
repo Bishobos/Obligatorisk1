bbbbed7b6d7c720e8496ef0b1104470b7e15a139
Bishobos <mps.kajkage@gmail.com>
2025-09-17 09:41:09 +0200

    Input Helper shell is done

--- src/library/LoanItems.java
+++ src/library/LoanItems.java
@@ -1,4 +1,17 @@
 package library;
 
 public class LoanItems {
+    String title;
+
+    public LoanItems(){
+        this.title = "title";
+    }
+    public LoanItems(String title){
+        this.title = title;
+    }
+
+    @Override
+    public String toString(){
+        return "Type\t" + this.title;
+    }
 }
